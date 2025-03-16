92424724bef8519f8d6256662a5fc1290021a0eb
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2025-03-16 10:53:20 -0700

    Beginnings of library and a simple demo to demonstrate it.

---
+++ SimpleSteamworksDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using SimpleSteamworks;
+
+namespace SimpleSteamworksDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Steamworks.GetSteamInstallPath();
+            if(string.IsNullOrEmpty(path))
+            {
+                Console.WriteLine("Steam installation not detected!");
+                return;
+            }
+            Console.WriteLine("Steam is installed here: {0}", path);
+
+            SteamLibraryPaths libs = new SteamLibraryPaths(path);
+            libs.LoadSteamLibraries();
+            if(libs.Paths.Count > 1)
+            {
+                Console.WriteLine("Steam Libraries are here:");
+            }
+            else
+            {
+                Console.WriteLine("Steam Library is here:");
+            }
+            libs.Paths.ForEach(Console.WriteLine);
+
+            AppManifestCache apps = new AppManifestCache(libs);
+            Console.WriteLine("App Manifest:");
+            // You can find Steam App IDs easily here if you don't know them: https://steamdb.info/
+            const int appId = 1549970; // This is Aliens: Fireteam Elite
+            Console.WriteLine("{0}: {1}", "appid", apps.GetManifestProperty(appId, "appid"));
+            Console.WriteLine("{0}: {1}", "name", apps.GetManifestProperty(appId, "name"));
+            Console.WriteLine("{0}: {1}", "installdir", apps.GetManifestProperty(appId, "installdir"));
+
+        }
+    }
+}
