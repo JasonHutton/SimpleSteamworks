ac0effdf369acf7261ba8a40f8459397f4ae1174
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2025-03-16 22:05:05 -0700

    Base cache class

---
+++ SimpleSteamworks/Cache.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace SimpleSteamworks
+{
+    public class Cache<TKey, TValue>
+    {
+        private readonly Dictionary<TKey, TValue> _cache = new Dictionary<TKey, TValue>();
+
+        public bool ContainsKey(TKey key)
+        {
+            return _cache.ContainsKey(key);
+        }
+
+        public TValue Get(TKey key)
+        {
+            _cache.TryGetValue(key, out TValue value);
+            return value;
+        }
+
+        public TValue this[TKey key]
+        {
+            get => Get(key);
+            set => Add(key, value);
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            _cache[key] = value;
+        }
+
+        public void InvalidateCache()
+        {
+            _cache.Clear();
+        }
+    }
+}
